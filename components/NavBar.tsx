1bc7cd4b2c9e89a7ccb90b2452a409493bf7d267
n0tg0d <khalilmell2020@gmail.com>
2025-08-15 19:38:48 +0100

    [feature} implementing navigation bar

---
+++ components/NavBar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image"
+import Link from "next/link"
+import NavItems from "./NavItems"
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <Link href="/">
+          <div className="flex items-center gap-2.5 cursor-pointer">
+            <Image src="/images/logo.svg"
+            alt="logo"
+            width={46} height={44}
+            />
+          </div>
+        </Link>
+        <div className="flex items-center gap-4">
+          <NavItems />
+          <p>Sign In</p>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
