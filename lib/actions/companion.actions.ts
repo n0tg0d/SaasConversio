fe8395f576acce7d2c18a8cb4aa274fe9087e388
n0tg0d <khalilmell2020@gmail.com>
2025-08-16 23:52:39 +0100

    [feature] Vapi setup and header

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -38,4 +38,17 @@ export const getAllCompanions = async ({ limit=10, page=1, subject, topic }: Get
     if(error) throw new Error(error.message);
 
     return companions;
-};
+}
+
+export const getCompanion = async (id: string) =>{
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0];
+}
