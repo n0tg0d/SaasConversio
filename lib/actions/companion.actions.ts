7daed50cbfbeeb58535543149ea9172a9e8e7490
n0tg0d <khalilmell2020@gmail.com>
2025-08-22 21:45:42 +0100

    [feature] add my journey page

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -92,3 +92,17 @@ export const getUserSessions = async (userId: string, limit = 10) => {
 
      return data.map(({ companions }) => companions);
 }
+
+export const getUserCompanions = async (userId: string) => {
+     const supabase = createSupabaseClient();
+     const {data, error} = await supabase
+     .from('companions')
+     .select()
+     .eq('author', userId)
+     .order('created_at', { ascending: false });
+     
+
+     if(error) throw new Error(error.message);
+
+     return data;
+}
